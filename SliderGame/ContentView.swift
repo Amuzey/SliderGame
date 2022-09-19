8b099ce4b7936d7be1d905205b69ca05b38f9634
Amuzey <ivanovad97@gmail.com>
2022-09-19 19:51:19 +0500

    create UI

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let targetValue = Int.random(in: 0...100)
+    @State var aletrPresent = false
+    @State private var currentValue: Double = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            HStack {
+                Text("Подвиньте слайдер, как можно ближе к: ")
+                Text(targetValue.formatted())
+            }
+            HStack {
+                Text("0")
+                Slider(value: $currentValue, in: 0...100, step: 1)
+                Text("100")
+            }
+            
+            Button("Проверь меня!") {
+                
+            }
+            Button("Начать заново") {
+                print("2")
+            }
+            Spacer()
+            Text(currentValue.formatted())
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
